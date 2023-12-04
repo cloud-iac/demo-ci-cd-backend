ee87bd378e1853ee4665ae82d40275f1c811af2f
w-ryan-jung <w.ryan.jung@gmail.com>
2023-12-05 02:20:27 +0900

    update

--- src/main/java/com/kimdragon/employeemanagement/repository/IEmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kimdragon.employeemanagement.repository;
-
-import com.kimdragon.employeemanagement.domain.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IEmployeeRepository extends JpaRepository<Employee, Long> {
-}
