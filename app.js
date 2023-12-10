d6ebbc8bd48563fda2070a5c1555174d55274326
w-ryan-jung <w.ryan.jung@gmail.com>
2023-12-10 12:48:36 +0900

    update

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, *');
   res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Content-Type', 'application/json');
   next();
 });
 app.options('*', cors());
