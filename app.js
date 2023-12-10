d3ed9e0edb6aae7c75dc8b7e8046b9aa9f33b7bb
w-ryan-jung <w.ryan.jung@gmail.com>
2023-12-10 11:59:56 +0900

    update

--- app.js
+++ app.js
@@ -8,14 +8,13 @@ import authRouter from "./router/auth.js";
 import healthzRouter from "./router/healthz.js";
 const app = express();
 
-const corsOptions = {
-  origin: 'http://front-tier-alb-1611244642.ap-northeast-2.elb.amazonaws.com', // Specify the allowed origin
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Specify the allowed HTTP methods
-  credentials: true, // Include credentials (cookies, HTTP authentication) in the CORS request
-  optionsSuccessStatus: 204, // Respond with a 204 status code for successful preflight requests
-};
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+app.options('*', cors());
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("tiny"));
