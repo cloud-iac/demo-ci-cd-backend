97d1fd6ef1c6dda4fcb53e170880af444309e823
w-ryan-jung <w.ryan.jung@gmail.com>
2023-12-18 18:14:28 +0900

    update

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
-import express from "express";
-import "express-async-errors";
-import cors from "cors";
-import morgan from "morgan";
-import helmet from "helmet";
-import tweetsRouter from "./router/tweets.js";
-import authRouter from "./router/auth.js";
-import healthzRouter from "./router/healthz.js";
+import express from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import tweetsRouter from './router/tweets.js';
+import authRouter from './router/auth.js';
+import { config } from './config.js';
+import { initSocket } from './connection/socket.js';
+import { sequelize } from './db/database.js';
+
 const app = express();
-import bodyParser from "body-parser";
 
-app.use(cors());
-// app.use(express.json());
-app.use(bodyParser.json());
+const corsOption = {
+  origin: config.cors.allowedOrigin,
+  optionsSuccessStatus: 200,
+}
+
+app.use(express.json());
 app.use(helmet());
-app.use(morgan("combined"));
+app.use(cors(corsOption));
+app.use(morgan('combined'));
 
-app.use("/tweets", tweetsRouter);
-app.use("/auth", authRouter);
-app.use("/healthz", healthzRouter);
+app.use('/tweets', tweetsRouter);
+app.use('/auth', authRouter);
+app.use('/healthz', ((req, res, next) => {
+  res.sendStatus(200);
+}));
+
 app.use((req, res, next) => {
   res.sendStatus(404);
 });
@@ -26,4 +35,9 @@ app.use((error, req, res, next) => {
   console.error(error);
   res.sendStatus(500);
 });
-app.listen(80);
+
+sequelize.sync().then(() => {
+  console.log(`Server is started... ${new Date()}`);
+  const server = app.listen(config.port);
+  initSocket(server);
+});
--- router/healthz.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import 'express-async-errors';
-const router = express.Router();
-router.get('/',(req, res, next) => {
-    res.sendStatus(200);
-  } );
-export default router;
